afaa5ed62badd059af8453e1de8ad6281e650ac5
Benedikt Poller <poller@kutech.at>
2016-02-28 10:19:36 +0100

    init

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({
+        redirectTo: '/view1'
+    });
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,110 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'LexinController'
+        })
+        .when('/aktuell', {
+            templateUrl: 'views/aktuell/aktuell.html',
+            controller: 'LexinController'
+        })
+        .when('/lexin', {
+            templateUrl: 'views/lexin/lexin.html',
+            controller: 'LexinController'
+        })
+        .when('/lexin/auxine', {
+            templateUrl: 'views/lexin/lexin_auxine.html',
+            controller: 'LexinController'
+        })
+        .when('/lexin/huminosaeuren', {
+            templateUrl: 'views/lexin/lexin_huminosaeuren.html',
+            controller: 'LexinController'
+        })
+        .when('/lexin/fulvosaeuren', {
+            templateUrl: 'views/lexin/lexin_fulvosaeuren.html',
+            controller: 'LexinController'
+        })
+        .when('/dosierung', {
+            templateUrl: 'views/dosierung/dosierung.html',
+            controller: 'LexinController'
+        })
+        .when('/downloads', {
+            templateUrl: 'views/downloads/downloads.html',
+            controller: 'LexinController'
+        })
+        .when('/kontakt', {
+            templateUrl: 'views/kontakt/kontakt.html',
+            controller: 'LexinController'
+        })
+        .when('/bestellen', {
+            templateUrl: 'views/bestellen.html',
+            controller: 'LexinController'
+        })
+        .when('/impressum', {
+            templateUrl: 'views/impressum.html',
+            controller: 'LexinController'
+        })
+        .when('/agb', {
+            templateUrl: 'views/agb.html',
+            controller: 'LexinController'
+        });
+
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('LexinController', ['$scope', '$http', function ($scope, $http) {
+
+    angular.extend($scope, {
+        resetContactForm: resetContactForm,
+        submitContactForm: submitContactForm
+
+    });
+
+    resetContactForm();
+    resetOrder();
+
+    ///////////////////////
+
+    function submitContactForm(ev) {
+
+        ev.preventDefault();
+
+        $http.post('http://formspree.io/benedikt.poller@gmail.com', $scope.contact)
+            .success(function () {
+                $scope.messageSent = true;
+            });
+    }
+
+    function submitOrder() {
+        $http.post('http://formspree.io/benedikt.poller@gmail.com', $scope.order)
+            .success(function () {
+                $scope.orderSent = true;
+            });
+    }
+
+    function resetOrder() {
+        $scope.order = {
+            _subject: 'Lexin | Bestellung'
+        };
+        $scope.orderSent = false;
+    }
+
+    function resetContactForm() {
+        $scope.contact = {
+            _subject: 'Lexin | Kontaktformular'
+        };
+        $scope.messageSent = false;
+    }
+
+    // Slider
+    $('.multiple-items').slick({
+        fade: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+    });
 
 }]);
